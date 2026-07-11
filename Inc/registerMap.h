9a7a74d6b9bafaa7f427d8c6389bd3c97eb6b1be
Rasya <m.rasyafawwaz@gmail.com>
2026-07-10 23:26:40 -0700

    added SPI

--- Inc/registerMap.h
+++ Inc/registerMap.h
@@ -4,6 +4,8 @@
 #define GPIOB_BASE 0x40020400
 #define RCC_BASE 0x40023800
 #define USART2_BASE 0x40004400
+#define USART1_BASE 0x40011000
+#define SPI1_BASE 0x40013000
 
 typedef struct {
 	volatile uint32_t MODER;
@@ -32,16 +34,22 @@ typedef struct {
 	volatile uint32_t RESERVED1[2];
 	volatile uint32_t AHB1ENR;
 	volatile uint32_t AHB2ENR;
+	volatile uint32_t RESERVED2[2];
 	volatile uint32_t APB1ENR;
 	volatile uint32_t APB2ENR;
+	volatile uint32_t RESERVED3[2];
 	volatile uint32_t AHB1LPENR;
 	volatile uint32_t AHB2LPENR;
+	volatile uint32_t RESERVED4[2];
 	volatile uint32_t APB1LPENR;
 	volatile uint32_t APB2LPENR;
+	volatile uint32_t RESERVED5[2];
 	volatile uint32_t BDCR;
 	volatile uint32_t CSR;
+	volatile uint32_t RESERVED6[2];
 	volatile uint32_t SSCGR;
 	volatile uint32_t PLLI2SCFGR;
+	volatile uint32_t RESERVED7;
 	volatile uint32_t DCKCFGR;
 
 } RCC_TypeDef;
@@ -56,3 +64,16 @@ typedef struct {
 	volatile uint32_t GTPR;
 
 } USART_TypeDef;
+
+typedef struct {
+	volatile uint32_t CR1;
+	volatile uint32_t CR2;
+	volatile uint32_t SR;
+	volatile uint32_t DR;
+	volatile uint32_t CRCPR;
+	volatile uint32_t RXCRCR;
+	volatile uint32_t TXCRCR;
+	volatile uint32_t I2SCFGR;
+	volatile uint32_t I2SPR;
+
+} SPI_TypeDef;
--- Src/main.c
+++ Src/main.c
@@ -3,25 +3,59 @@
 #define RCC ((RCC_TypeDef*) RCC_BASE)
 #define GPIOA ((GPIO_TypeDef*) GPIOA_BASE)
 #define USART2 ((USART_TypeDef*) USART2_BASE)
+#define SPI1 ((SPI_TypeDef*) SPI1_BASE)
 
 
 // helper function to initialize USART from bare metal.
 void UART_Init() {
 	RCC->APB1ENR |= (1U << 17);
-	USART2->CR1 |= (1U << 13);
+	RCC->AHB1ENR |= (1U << 0);
+	GPIOA->MODER &= ~(1U << 4);
+	GPIOA->MODER |= (1U << 5);
+	GPIOA->MODER &= ~(1U << 6);
+	GPIOA->MODER |= (1U << 7);
+	GPIOA->AFRL |= (7U << 8);
+	GPIOA->AFRL |= (7U << 12);
 	USART2->CR1 |= (1U << 3);
 	USART2->CR1 &= ~(1U << 12);
 	USART2->BRR = 0x0000008B;//we are setting the whole register since the bits in BRR are mathematically linked with each other.
+	USART2->CR1 |= (1U << 13);
 }
 
 void GPIOA_Init() {
 	RCC->AHB1ENR |= (1U << 0);
 	GPIOA->MODER |= (1U << 10);
 	GPIOA->MODER &= ~(1U << 11);
+	GPIOA->OSPEEDR |= (3U << 10);
 }
 
+void SPI1_Init() {
+	RCC->AHB1ENR |= (1U << 0);
+	RCC->APB2ENR |= (1U << 12);
+
+	GPIOA->MODER &= ~((3U << 10) | (3U << 12) | (3U << 14));
+	GPIOA->MODER |=  ((2U << 10) | (2U << 12) | (2U << 14));
+
+	GPIOA->AFRL &= ~((0xFU << 20) | (0xFU << 24) | (0xFU << 28));
+	GPIOA->AFRL |=  ((5U << 20) | (5U << 24) | (5U << 28));
+
+	GPIOA->OSPEEDR = 0;
+	GPIOA->OSPEEDR |= (3U << 10) | (3U << 12) | (3U << 14);
+
+	SPI1->CR1 = 0;
+	SPI1->CR1 |= (1U << 2);            // MSTR
+	SPI1->CR1 |= (1U << 9) | (1U << 8); // SSM + SSI
+	SPI1->CR1 |= (6U << 3);            // BR: /256 for bring-up
+	SPI1->CR1 |= (1U << 6);            // SPE last
+}
+void set_bit(volatile uint32_t *reg, uint32_t pos, uint32_t width, uint32_t value) {
+	uint32_t copy = *reg;
+	copy &= ~(((1U << width) - 1) << pos);
+	copy |= (value << pos);
+	*reg = copy;
+}
 // helper function to transmit data in USART.
-void UART_Transmit_Data(char *word) {
+void UART_Transmit_Data(char * word) {
 	while(*word != '\0') {
 
 		while (!(USART2->SR & (1 << 7)));
@@ -34,15 +68,26 @@ void UART_Transmit_Data(char *word) {
 	while (!(USART2->SR & (1 << 6)));
 }
 
-int main () {
+void SPI1_Transmit(uint8_t data) {
+	while (!(SPI1->SR & (1U << 1)));
+//	&SPI1->DR;// wait TXE
+	SPI1->DR = data;
+	while (SPI1->SR & (1U << 7));     // wait until not BSY
+}
+
+	int main () {
 	GPIOA_Init();
-//	UART_Init();
-//	char *send = "Hello World!\n";
+	UART_Init();
+	char *send = "Hello World!\r\n";
+	SPI1_Init();
+	char byte = 0x50;
 
 
 	for (;;) {
 		GPIOA->ODR ^= (1U << 5);
-//		UART_Transmit_Data(send);
+		UART_Transmit_Data(send);
+		for (int i = 0; i < 1000000; i++);
+		SPI1_Transmit(byte);
 	}
 	return 0;
 
